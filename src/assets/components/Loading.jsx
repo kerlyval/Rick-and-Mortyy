2da79546e037b7f6c234ba2eace697d585d2954c
KerlyVal <182720458+kerlyval@users.noreply.github.com>
2024-11-13 20:53:47 -0500

    cambio

--- src/assets/components/Loading.jsx
+++ src/assets/components/Loading.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './Loading.css';
-import portalImage from '../assets/images/portal-a.png';
 
 const Loading = () => {
 	return (
 		<div className="loader">
 			<div className="svg-wrapper">
-				<img src={portalImage} alt="Loading..." className="loading-img" />
+				<img
+					src="/public/portal-a.png"
+					alt="Loading..."
+					className="loading-img"
+				/>
 
 				{/* <div className="loading-text">Welcome to this Galaxy </div> */}
 			</div>
